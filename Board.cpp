51592aaf98cd14f5a73f232605d6bc99b424ee09
Nathan Lou <natloudev@gmail.com>
2022-05-16 12:17:34 -0700

    finished mediocre player, started board

--- Board.cpp
+++ Board.cpp
@@ -21,13 +21,27 @@ class BoardImpl
   private:
       // TODO:  Decide what private members you need.  Here's one that's likely
       //        to be useful:
+    int board[MAXROWS][MAXCOLS]; 
     const Game& m_game;
+    int m_cols;
+    int m_rows;
+
+    vector<int> existingShipIDs;
 };
 
 BoardImpl::BoardImpl(const Game& g)
  : m_game(g)
 {
-    // This compiles, but may not be correct
+    m_rows = g.rows(); 
+    m_cols = g.cols();
+
+    for (int i = 0; i < m_rows; i++)
+    {
+        for (int j = 0; j < m_cols; j++)
+        {
+            board[i][j] = 0; // board states are initialized 
+        }
+    }
 }
 
 void BoardImpl::clear()
@@ -67,7 +81,173 @@ bool BoardImpl::unplaceShip(Point topOrLeft, int shipId, Direction dir)
 
 void BoardImpl::display(bool shotsOnly) const
 {
-    // This compiles, but may not be correct
+    // array dimensions must be constant for now, but we will alter it. 
+    char display[MAXCOLS + 3][MAXROWS + 1];
+
+    for (int c = 0; c < m_rows; c++) //for cols numbering, rows = 2
+    {
+        display[0][c + 1] = '0' + c;
+    }
+    for (int c = 1; c < m_rows + 1; c++)
+    {
+        display[1][c] = ' ';
+    }
+    for (int c = 0; c < m_cols; c++) //numbering for rows. row is 3 size.
+    {
+        display[c + 2][0] = '0' + c;
+    }
+
+    //nrows=2
+    //ncols=3
+    //cout<<endl; the implementation sampel program doesnt have this line of code.
+
+
+
+    cout << "  ";//2 spaces?
+
+    for (int c = 0; c < m_rows; c++)
+    {
+        for (int r = 0; r < m_cols; r++)
+        {
+            if (board[r][c] == -1) //for mediocre players
+            {
+                display[r + 2][c + 1] = '#'; //means blocked.
+            }
+            if (board[r][c] == 0) 
+            {
+                display[r + 2][c + 1] = '.';
+            }
+            if (board[r][c] == 1) 
+            {
+                display[r + 2][c + 1] = 'o';
+
+            }
+            if (board[r][c] > 1) 
+            {
+                display[r + 2][c + 1] = 'X';
+            }
+        }
+    }
+
+
+
+    if(!shotsOnly)
+    {
+        for (int c = 0; c < m_rows; c++)
+        {
+            for (int r = 0; r < m_cols; r++)
+            {
+
+                for (int d = 0; d < m_game.nShips(); d++)
+                {
+                    if (display[r][c] == d)
+                    {
+                        //actually after palcing all the hsips, its just the index of the shipsin the vector, which i have a size function to keep track of.
+                        if (existingShipIDs.size() > d)
+                        {
+                            display[r + 2][c + 1] = m_game.shipSymbol(existingShipIDs[d]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+
+    //if a ship is DAMAGED we must dispaly it as X instead.
+    /*
+     Remaining lines: The function must print a digit specifying the row number,
+     starting at 0, followed by a space, followed by the contents of the current row,
+     followed by a newline. You may assume there will be no more than 10 rows. In
+     each of the positions of the row, use the following characters to represent the
+     playing field:
+     a. If the shotOnly parameter is false, use the ship's symbol to display an
+     undamaged ship segment; if the shotsOnly parameter is true, show a
+     period to display an undamaged ship segment.
+     b. Use an X character to display any damaged ship segment.
+     c. Use a period to display water where no attack has been made.
+     d. Use a lower case letter o character to display water where an attack has
+     been made that missed a ship.
+     */
+
+     //for hsotsonly it literally means, SHOTS ONLY LOL. so your literally only seeing water or if i hit a ship.
+
+     //now use the shotsonly test to see if I shouldp lace a o or an X, X for displayBoard[r][c]!=0, and true, and a o for displayBoard[r][c]==0
+     //basically the code below now doesn't seem necssary at all, ill take it out if it works.
+    for (int c = 0; c < m_rows; c++)
+    {
+        for (int r = 0; r < m_cols; r++)
+        {
+
+            if (board[r][c] > 1000) //a ship!
+            { //so a ship. I should verify that my r and c is not going to be hitting any of the 0-9 on the cout board.
+                display[r + 2][c + 1] = 'X';
+            }
+            if (board[r][c] == 1000) //a hit water.
+            {
+                display[r + 2][c + 1] = 'o';
+            }
+            if (board[r][c] == -2) //for mediocre players
+            {
+                display[r + 2][c + 1] = '#'; //means blocked.
+            }
+        }
+        //endl;
+    }
+
+    //how about just straight up hardcode first row, ignore the first row here, and then print rest
+
+    if (!shotsOnly) //everytjing allowed
+    {
+        for (int c = 0; c < m_rows + 1; c++) //1 space with number of rows
+        {
+            for (int r = 0; r < m_cols + 2; r++) //should be 2 spaces due to number of cols
+            {
+                if (c == 0 && r < 2) //r>9
+                {
+                    //cout<<r<<endl;
+                    continue;
+                }
+
+
+                cout << display[r][c];
+            }
+            cout << endl;
+        }
+    }
+    if (shotsOnly) //everytjing not allowed
+    {
+        for (int c = 0; c < m_rows + 1; c++) //1 space with number of rows
+        {
+            for (int r = 0; r < m_cols + 2; r++) //should be 2 spaces due to number of cols
+            {
+                if (c == 0 && r < 2) //r>9
+                {
+                    //cout<<r<<endl;
+                    continue;
+                }
+                if (r > 1 && c > 0)
+                {
+                    if (display[r][c] == 'X' || display[r][c] == 'o' || display[r][c] == '.')
+                    {
+                        cout << display[r][c];
+                    }
+                    else
+                    {
+                        cout << '.';//its just a normal water if we don't know what it is.
+                    }
+                }
+                else
+                {
+                    cout << display[r][c]; //prints out the number of row and col
+                }
+
+            }
+            cout << endl;
+        }
+    }
+
 }
 
 bool BoardImpl::attack(Point p, bool& shotHit, bool& shipDestroyed, int& shipId)
--- Player.cpp
+++ Player.cpp
@@ -4,7 +4,8 @@
 #include "globals.h"
 #include <iostream>
 #include <string>
-
+#include <stack>
+#include <vector>
 using namespace std;
 
 //*********************************************************************
@@ -156,7 +157,7 @@ void HumanPlayer::recordAttackByOpponent(Point /* p */) {}
 class MediocrePlayer : public Player
 {
 public:
-    MediocrePlayer(string nm, const Game& g) : Player(nm, g)  {}
+    MediocrePlayer(string nm, const Game& g) : Player(nm, g), m_lastCellAttacked(0, 0) { state1 = true; }
     virtual ~MediocrePlayer() {}
     virtual bool placeShips(Board& b);
     virtual Point recommendAttack();
@@ -164,25 +165,70 @@ public:
         bool shipDestroyed, int shipId);
     virtual void recordAttackByOpponent(Point p);
 private:
-    bool recursivePlacements(Board& b);
-
+    bool DFS(Board& b, Point p, int id, stack<Point> current_ships);
+    Point m_lastCellAttacked;
+    vector<Point> attackedCells;
+    vector<Point> limitedCells;
+    vector<Point> attackedLimitedCells;
+    Point state2_point;
+    bool state1;
 };
 
 // TODO:  You need to replace this with a real class declaration and
 //        implementation.
 
-bool MediocrePlayer::recursivePlacements(Board& b)
+bool MediocrePlayer::DFS(Board& b, Point p, int id, stack<Point> current_ships)
 {
+    if (id == game().nShips()) return true; //return true if all the ships have been placed 
+
+    if (p.c == game().cols())
+    {
+        return DFS(b, Point(p.r + 1, 0), id, current_ships); // if the column is past the last one, it goes to the next row
+    }
+
+    if (p.r == game().rows()) // if the last row was passed, then it does something 
+    {
+        if (id == 0) {
+            return false; // if it backtracks to the first ship, or the first ship cannot find a place to be placed anymore, then it returns false 
+        }
+        
+        Point backtrack = current_ships.top(); // backtracks
+
+        current_ships.pop(); 
+
+        return DFS(b, Point(backtrack.r, backtrack.c + 1), id - 1, current_ships); // tries to replace the last ship placed (in the stack)
+
+    }
+
+    if (b.placeShip(p, id, HORIZONTAL)) // tries horizontal placement 
+    {
+        current_ships.push(p);
+        DFS(b, Point(0, 0), id + 1, current_ships);
+    } 
+    else if (b.placeShip(p, id, VERTICAL))
+    {
+        current_ships.push(p);
+        DFS(b, Point(0, 0), id + 1, current_ships);
+    }
+    
+    return DFS(b, Point(p.r, p.c + 1), id, current_ships); // next column 
 
 }
 
 bool MediocrePlayer::placeShips(Board& b)
 {
+
+
     for (int i = 0; i < 50; i++)
     {
         b.block();
 
+        stack<Point> empty_stack;
 
+        if (DFS(b, Point(0, 0), 0, empty_stack))
+        {
+            return (true);
+        }
 
         b.unblock();
     }
@@ -190,6 +236,248 @@ bool MediocrePlayer::placeShips(Board& b)
     return (false);
 }
 
+void MediocrePlayer::recordAttackByOpponent(Point p) {} // does nothing 
+
+Point MediocrePlayer::recommendAttack() 
+{
+    if (state1)
+    {
+        m_lastCellAttacked = game().randomPoint();
+
+        if (attackedCells.size() > 0)
+        {
+            bool unique = false;
+            while (!unique)
+            {
+                m_lastCellAttacked = game().randomPoint(); 
+                for (int c = 0; c < attackedCells.size(); c++)
+                {
+                    if (m_lastCellAttacked.r == attackedCells[c].r && m_lastCellAttacked.c == attackedCells[c].c)
+                    {
+                        unique = false;
+                        break;
+                    }
+                    else
+                    {
+                        unique = true;
+
+                    }
+                }
+            }
+        }
+    }
+    if (!state1) // currently in state2 
+    {
+        if (limitedCells.size() > 0)
+        {
+            int count = 0;
+            for (int c = 0; c < attackedLimitedCells.size(); c++)
+            {
+                for (int d = 0; d < limitedCells.size(); d++)
+                {
+                    if (attackedLimitedCells[c].r == limitedCells[d].r && attackedLimitedCells[c].c == limitedCells[d].r)
+                    {
+                        count++;
+                    }
+                }
+            }
+            if (count >= limitedCells.size())
+            {
+                state1 = true;
+                return recommendAttack(); //recursively attack
+            }
+
+        }
+
+        if (attackedCells.size() > 0)
+        {
+
+            bool unique = false;
+            bool valid = true;
+            while (!unique)
+            {
+                valid = true;
+                m_lastCellAttacked = game().randomPoint();
+
+                for (int c = 0; c < limitedCells.size(); c++)
+                {
+                 
+                    if (m_lastCellAttacked.r == limitedCells[c].r && m_lastCellAttacked.c == limitedCells[c].c)
+                    {
+ 
+                        if (attackedLimitedCells.size() > 0)
+                        {
+                            for (int d = 0; d < attackedLimitedCells.size(); d++)
+                            {
+                                if (attackedLimitedCells[d].c == m_lastCellAttacked.c && attackedLimitedCells[d].r == m_lastCellAttacked.r)
+                                {
+                                    valid = false;
+                                }
+                            }
+                        }
+                        if (valid)
+                        {
+                            attackedLimitedCells.push_back(m_lastCellAttacked);
+                            unique = true;
+                            break; 
+                        }
+                    }
+                    else
+                    {
+                        unique = false;
+                    }
+                }
+            }
+        }
+
+    }
+    return m_lastCellAttacked;
+}
+
+void MediocrePlayer::recordAttackResult(Point p, bool validShot, bool shotHit,
+    bool shipDestroyed, int shipId) 
+{
+    attackedCells.push_back(p); //so i dont hit again
+    bool justSwitched = false;
+    if (state1)
+    {
+        if (!shotHit)
+        {
+            state1 = true;
+        }
+        if (shotHit && shipDestroyed)
+        {
+            state1 = true;
+        }
+        else if (shotHit && !shipDestroyed)
+        {
+            state1 = false;
+            justSwitched = true;
+        }
+    }
+    if (!state1)
+    {
+        /*In state 2, let (r,c) be the position that was hit that caused the transition from state
+         1 to state 2. In state 2, recommendAttack returns a randomly chosen position
+         drawn from a limited set: the set of valid positions no more than 4 steps away
+         from (r,c) either horizontally or vertically, that have not been chosen before. This
+         set defines a cross around (r,c). For example, assume that in state 1, a shot was
+         fired on position (5,3) that hit a ship, but did not destroy it, resulting in:
+         */
+        if (justSwitched)
+        {
+            // cout<<"YOU FUKING TRIGGERED ME"<<endl;
+
+            if (limitedCells.size() > 0)
+            {
+                for (int c = 0; c < limitedCells.size(); c++)
+                {
+                    limitedCells.pop_back();
+                }
+            }
+
+            state2_point = p; //4 4   0  0
+            int rS2Left = state2_point.r - 4; //4 0    - 4 0    (my row left so -4 off grid
+            int rS2Right = state2_point.r + 4;  //4 8     8  0
+            int cS2Top = state2_point.c - 4; //0 4     -4   4
+            int cS2Bot = state2_point.c + 4; // 8 4
+
+            attackedLimitedCells.push_back(state2_point);
+
+
+            //run a check to see that if these values are also in the point of attacked cells. if they are, then dont even bother trying to push it in as a coordinate. also, check if the coordinate to push in is a valid one to begin with. if this is implemented right, the above wouldn't have to worry about iterators at all.
+            for (int c = rS2Left; c <= rS2Right; c++) //-4 -3 -2 -1 0 1 2 3
+            {
+                bool canpush = true;
+                for (int d = 0; d < attackedCells.size(); d++)
+                {//if attacked cells
+             //1 0
+                    if (attackedCells[d].r == c && attackedCells[d].c == state2_point.c)
+                    {
+                        canpush = false;
+                    }
+                    else
+                    {
+
+                    }
+                }
+                Point tryingtoPush;
+                tryingtoPush.r = c;
+                tryingtoPush.c = state2_point.c;
+                if (!game().isValid(tryingtoPush))
+                {
+                    canpush = false;
+                }
+
+                if (canpush)
+                {
+                    limitedCells.push_back(Point(c, state2_point.c));//coordinates for left to right
+                }
+            }
+            for (int c = cS2Top; c <= cS2Bot; c++)
+            {
+                bool canpush = true;
+                for (int d = 0; d < attackedCells.size(); d++)
+                {//if attacked cells
+
+                    if (attackedCells[d].c == c && attackedCells[d].r == state2_point.r)
+                    {
+                        canpush = false;
+                    }
+                    else
+                    {
+
+                    }
+                }
+                Point tryingtoPush;
+                tryingtoPush.r = c;
+                tryingtoPush.c = state2_point.r;
+                if (!game().isValid(tryingtoPush))
+                {
+                    canpush = false;
+                }
+                if (canpush)
+                {
+                    limitedCells.push_back(Point(state2_point.r, c));//top to bot
+
+                }
+
+                //limitedCells.push_back(Point(c,PointofState2.c));//coordinates for left to right
+                //oh shoot if theres a new one u have to pop_back all the limited cells so u dont bait it .
+            }
+
+
+            //last but not least, if all possible checks in that area can be checked is done for, then i have to say to go back to random selection.
+
+        }
+        else
+        {
+            //  cout<<"Resuming state 2 barage"<<endl;
+            if (!shotHit)//miss
+            {
+                state1 = false;
+            }
+            else if (shotHit && !shipDestroyed)
+            {
+                state1 = false;
+            }
+            else if (shotHit && shipDestroyed)
+            {
+                state1 = true; //i go back to normal
+            //    cout<<"Since i sank ur ship im going back to normal me."<<endl;
+                /*
+                for(int c=0;c<attackedLimitedCells.size();c++)
+                {
+                    attackedLimitedCells.pop_back();
+                }
+                 */
+            }
+        }
+    }
+
+
+
+}
 // Remember that Mediocre::placeShips(Board& b) must start by calling
 // b.block(), and must call b.unblock() just before returning.
 
